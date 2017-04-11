2a43329e379ee742ffe72e2c06c4a09df3dea691
Sharmila Velamur <sharmila@chargebackgurus.com>
2017-04-10 18:07:46 -0600

    first commit

---
+++ assets/js/cytoscape-cose-bilkent/src/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// registers the extension on a cytoscape lib ref
+var getLayout = require('./Layout');
+
+var register = function( cytoscape ){
+  var Layout = getLayout( cytoscape );
+
+  cytoscape('layout', 'cose-bilkent', Layout);
+};
+
+if( typeof cytoscape !== 'undefined' ){ // expose to global cytoscape (i.e. window.cytoscape)
+  register( cytoscape );
+} else if( typeof define !== 'undefined' && define.amd ){
+  define( [], register );
+}
+
+module.exports = register;
