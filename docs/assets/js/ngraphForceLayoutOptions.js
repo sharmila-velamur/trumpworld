fc788ec9c29b810c442f66224db5775e67e2c807
Sharmila Velamur <sharmila@chargebackgurus.com>
2017-04-18 21:11:00 -0600

    better force directed layout

--- docs/assets/js/ngraphForceLayoutOptions.js
+++ docs/assets/js/ngraphForceLayoutOptions.js
@@ -2,28 +2,28 @@ var layoutOptions = {
                 name: 'cytoscape-ngraph.forcelayout',
                 async: {
                     // tell layout that we want to compute all at once:
-                    maxIterations: 5000,
+                    maxIterations: 1000,
                     stepsPerCycle: 30,
 
                     // Run it till the end:
-                    waitForStep: true
+                    waitForStep: false
                 },
                 physics: {
                     /**
                      * Ideal length for links (springs in physical model).
                      */
-                    springLength: 6000,
+                    springLength: 30,
 
                     /**
                      * Hook's law coefficient. 1 - solid spring.
                      */
-                    springCoeff: 0.00000001,
+                    springCoeff: 0.000008,
 
                     /**
                      * Coulomb's law coefficient. It's used to repel nodes thus should be negative
                      * if you make it positive nodes start attract each other :).
                      */
-                    gravity: -15.0,
+                    gravity: -20.2,
 
                     /**
                      * Theta coefficient from Barnes Hut simulation. Ranged between (0, 1).
@@ -37,24 +37,24 @@ var layoutOptions = {
                      * Drag force coefficient. Used to slow down system, thus should be less than 1.
                      * The closer it is to 0 the less tight system will be.
                      */
-                    dragCoeff: 0.0001,
+                    dragCoeff: 0.000002,
 
                     /**
                      * Default time step (dt) for forces integration
                      */
                     timeStep: 20,
-                    iterations: 5000,
+                    iterations: 10000,
                     fit: false,
 
                     /**
                      * Maximum movement of the system which can be considered as stabilized
                      */
-                    stableThreshold: 0.001
+                    stableThreshold: 0.000009
                 },
-                iterations: 5000,
-                refreshInterval: 50, // in ms
-                refreshIterations: 100, // iterations until thread sends an update
-                stableThreshold: 5,
-                animate: true,
-                fit: false //True makes it always resize the entire graph in the window. This seems to free up memory for rendering and peform better
+                animate: false,
+                iterations: 10000,
+                refreshInterval: 16, // in ms
+                refreshIterations: 10, // iterations until thread sends an update
+                stableThreshold: 2,
+                fit: false
             };
